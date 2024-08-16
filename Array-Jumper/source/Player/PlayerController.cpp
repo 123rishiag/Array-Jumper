1fa354c1b67d3d272693b5c9130d069f6dfd7338
Rishabh Garg <narishabh.garg@gmail.com>
2024-08-16 18:46:27 +0530

    Implemented Player Movement

--- Array-Jumper/source/Player/PlayerController.cpp
+++ Array-Jumper/source/Player/PlayerController.cpp
@@ -1,13 +1,24 @@
 #include "../../header/Player/PlayerController.h"
 #include "../../header/Player/PlayerView.h"
 #include "../../header/Player/PlayerModel.h"
+#include "../../header/Player/MovementDirection.h"
+#include "../../header/Level/LevelData.h"
+#include "../../header/Global/ServiceLocator.h"
+#include "../../header/Sound/SoundService.h"
+#include "../../header/Event/EventService.h"
 
 namespace Player
 {
+	using namespace Global;
+	using namespace Sound;
+	using namespace Level;
+	using namespace Event;
+
 	PlayerController::PlayerController()
 	{
 		player_model = new PlayerModel();
 		player_view = new PlayerView(this);
+		event_service = nullptr;
 	}
 
 	PlayerController::~PlayerController() { destroy(); }
@@ -16,16 +27,63 @@ namespace Player
 	{
 		player_model->initialize();
 		player_view->initialize();
+
+		event_service = ServiceLocator::getInstance()->getEventService();
 	}
 
 	void PlayerController::update()
 	{
 		player_view->update();
+		readInput();
 	}
 
 	void PlayerController::render()
 	{
 		player_view->render();
+	}
+
+	bool PlayerController::isPositionInBound(int targetPosition) const
+	{
+		if (targetPosition >= 0 && targetPosition < LevelData::NUMBER_OF_BOXES)
+			return true;
+		return false;
+	}
+
+	void PlayerController::move(MovementDirection direction)
+	{
+		int steps, targetPosition;
+		switch (direction)
+		{
+		case MovementDirection::FORWARD:
+			steps = 1;
+			break;
+		case MovementDirection::BACKWARD:
+			steps = -1;
+			break;
+		default:
+			steps = 0;
+			break;
+		}
+
+		targetPosition = player_model->getCurrentPosition() + steps;
+
+		if (!isPositionInBound(targetPosition))
+			return;
+
+		player_model->setCurrentPosition(targetPosition);
+		ServiceLocator::getInstance()->getSoundService()->playSound(SoundType::MOVE);
+	}
+
+	void PlayerController::readInput()
+	{
+		if (event_service->pressedRightArrowKey() || event_service->pressedDKey())
+		{
+			move(MovementDirection::FORWARD);
+		}
+		if (event_service->pressedLeftArrowKey() || event_service->pressedAKey())
+		{
+			move(MovementDirection::BACKWARD);
+		}
 	}
 
 	PlayerState PlayerController::getPlayerState()
